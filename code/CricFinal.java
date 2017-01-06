33836dff3e330c3de2455648d6251c9821d38354
Poornav <poornavsargoor@gmail.com>
2017-01-05 19:45:41 -0800

    Added comments

--- code/CricFinal.java
+++ code/CricFinal.java
@@ -1,3 +1,5 @@
+/* JSoup Code to extract the commentary of the world cup final 2011 cricket match which was played between India and Sri Lanka */
+
 import java.util.*;
 import java.io.*;
 import org.jsoup.Jsoup;
@@ -12,12 +14,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 public class CricFinal {
 	public static void main(final String[] args) throws IOException {
-		//System.out.println("\nhere i am"+args[0]);
+		/*connect to http://www.cricbuzz.com*/
 		Document doc = Jsoup.connect("http://www.cricbuzz.com/cricket-scores/2215/ind-vs-sl-final-icc-world-cup-2011").userAgent("Mozilla").timeout(20000).get();
-		//Elements commentary = doc.select("div.col-sm-12.col-xs-12.col-md-8.col-lg-8.bg-white.cb-comtry-col div.row p.col-sm-11.col-md-11.col-xs-11.col-lg-11.cb-font-14.cb-comm-text");
+		/*Extract the data from a specific <div> element*/
 		Elements overs = doc.select("div.col-sm-12.col-xs-12.col-md-8.col-lg-8.bg-white.cb-comtry-col div.row span.col-sm-1.col-md-1.col-xs-1.col-lg-1.cb-font-14");
 		int length=overs.size();
-		//System.out.println(length);
+		/*print it out on the console or file*/
 		for(int i=0;i<length;i++)
 		{
 			System.out.println(overs.get(i).text());
--- code/Duckworth_Lewis.py
+++ code/Duckworth_Lewis.py
@@ -1,7 +1,8 @@
 import collections
 import copy 
-import math
+
 from prettytable import PrettyTable
+
 def DLMethod(oppScore,scoreDict):
     for key,value in scoreDict.items():
         score=value[0]
@@ -28,67 +29,6 @@ def DLMethod(oppScore,scoreDict):
                 print("Prediction at "+str(key)+":Team batting second will win")
             else:
                 print("Prediction at "+str(key)+":Team batting second will lose")
-
-
-
-def batsmanClassification(batsmen):
-    res={}
-    weight=[3,1.5,5]
-    for key,value in batsmen.iteritems():
-        rating=0
-        for i in range(len(value)):
-            if (value[i][1]<5):
-                #print key
-                rating+=(math.pow(value[i][0],2))/((value[i][1]+.01)*50)
-            else:
-                rating+=(math.pow(value[i][0],2))/((value[i][1]+.01)*10)
-        if(rating < 2.5):
-            res[key] = "Bad"
-        elif(rating <7):
-            res[key] = "Average"
-        else:       
-            res[key] = "Good"
-
-    t=PrettyTable(["BATSMAN","PERFORMANCE"])
-
-    for key,val in res.iteritems():
-        t.add_row([key,val])
-    print t
-
-
-
-
-
-
-def bowlerClassification(bowlers):
-    res={}
-    weight=[3,1.5,5]
-    for key,value in bowlers.iteritems():
-        rating=0
-        for i in range(len(value)):
-            if(value[i][1]!=0):
-                    rating+=((value[i][2]+1)*weight[i])/(value[i][1]/(value[i][0]+0.01)+0.01)
-                #print rating
-        if(rating < 1):
-            res[key] = "Bad"
-        elif(rating < 3.5):
-            res[key] = "Average"
-        else:       
-            res[key] = "Good"
-
-        
-    t=PrettyTable(["BOWLER","PERFORMANCE"])
-
-    for key,val in res.iteritems():
-        t.add_row([key,val])
-    print t
-
-
-
-
-
-
-
 
 f=open("CricFinal.txt",'r')
 #print f.read()
@@ -135,88 +75,15 @@ Sec_details=collections.OrderedDict()
 bowler_first=collections.OrderedDict()
 batsmen_first=collections.OrderedDict()
 
-bowler_dum=collections.OrderedDict()
-batsmen_dum=collections.OrderedDict()
-
-batsmen_fclass=collections.OrderedDict()
-batsmen_sclass=collections.OrderedDict()
-bowler_fclass=collections.OrderedDict()
-bowler_sclass=collections.OrderedDict()
 
 scores=[]
 ############Overs calculation ENDS###################
 target=0
 j=0
-no_ovrs=0
-#print one[int(309)]
 for i in range(1,len(one)):
-    #print no_ovrs,i
     if(i==int(end)):
-        no_ovrs+=1
+        count+=1
         bowler_details[prev][0]+=1
-    if(no_ovrs==10 and checkf=="yes"):
-        for k in bowler_details:
-            bowler_fclass[k]=[[0,0,0],[0,0,0],[0,0,0]]
-            bowler_fclass[k][0][0]=bowler_details[k][0]
-            bowler_fclass[k][0][1]=bowler_details[k][1]
-            bowler_fclass[k][0][2]=bowler_details[k][2]
-        for k in batsmen_details:
-            batsmen_fclass[k]=[[0,0],[0,0],[0,0]]
-            batsmen_fclass[k][0][0]=batsmen_details[k][0]
-            batsmen_fclass[k][0][1]=batsmen_details[k][3]
-           
-        checkf="no"
-
-    elif(no_ovrs==35 and checkf=="yes"):
-        #print i
-        for k in bowler_details:
-            try:
-                
-                bowler_fclass[k][1][0]=bowler_details[k][0]-bowler_fclass[k][0][0]
-                bowler_fclass[k][1][1]=bowler_details[k][1]-bowler_fclass[k][0][1]
-                bowler_fclass[k][1][2]=bowler_details[k][2]-bowler_fclass[k][0][2]
-            except KeyError:
-                bowler_fclass[k]=[[0,0,0],[0,0,0],[0,0,0]]
-                bowler_fclass[k][1][0]=bowler_details[k][0]
-                bowler_fclass[k][1][1]=bowler_details[k][1]
-                bowler_fclass[k][1][2]=bowler_details[k][2]
-        for k in batsmen_details:
-            try:
-                
-                batsmen_fclass[k][1][0]=batsmen_details[k][0]-batsmen_fclass[k][0][0]
-                batsmen_fclass[k][1][1]=batsmen_details[k][3]-batsmen_fclass[k][0][1]
-            except KeyError:
-                batsmen_fclass[k]=[[0,0],[0,0],[0,0]]
-                batsmen_fclass[k][1][0]=batsmen_details[k][0]
-                batsmen_fclass[k][1][1]=batsmen_details[k][3]
-        checkf="no"
-    elif((no_ovrs==50 or i==len(one)-1)and checkf=="yes"):
-        #print i
-        for k in bowler_details:
-            try:
-                bowler_fclass[k][2][0]=bowler_details[k][0]-bowler_fclass[k][0][0]-bowler_fclass[k][1][0]
-                bowler_fclass[k][2][1]=bowler_details[k][1]-bowler_fclass[k][0][1]-bowler_fclass[k][1][1]
-                bowler_fclass[k][2][2]=bowler_details[k][2]-bowler_fclass[k][0][2]-bowler_fclass[k][1][2]
-            except KeyError:
-                bowler_fclass[k]=[[0,0,0],[0,0,0],[0,0,0]]
-                bowler_fclass[k][2][0]=bowler_details[k][0]
-                bowler_fclass[k][2][1]=bowler_details[k][1]
-                bowler_fclass[k][2][2]=bowler_details[k][2]
-        for k in batsmen_details:
-            try:
-                
-                batsmen_fclass[k][2][0]=batsmen_details[k][0]-batsmen_fclass[k][0][0]-batsmen_fclass[k][1][0]
-                batsmen_fclass[k][2][1]=batsmen_details[k][3]-batsmen_fclass[k][0][1]-batsmen_fclass[k][1][1]
-            except KeyError:
-                batsmen_fclass[k]=[[0,0],[0,0],[0,0]]
-                batsmen_fclass[k][2][0]=batsmen_details[k][0]
-                batsmen_fclass[k][2][1]=batsmen_details[k][3]
-        checkf="no"
-        no_ovrs=0
-    if(i==int(end)):
-        no_ovrs=0
-        
-        
         bowler_first= copy.deepcopy(bowler_details)
         batsmen_first= copy.deepcopy(batsmen_details)
         bowler_details.clear()
@@ -225,17 +92,15 @@ for i in range(1,len(one)):
         target=SecScore
         SecWickets=0
         SecScore=0
-    #print batsmen_details
-    #print bowler_details
-    #print i,one[i]
+        
     
     split_one=one[i].split(',')
     bat_bowl=split_one[0]
-    #print one[i]
+    
     score=split_one[1]
     bowler=bat_bowl.split("to")[0]
     batsmen=bat_bowl.split("to")[1]
-    #print "!",score,"!"
+    
     try:
         bowler_details[bowler]
     except KeyError:
@@ -271,14 +136,14 @@ for i in range(1,len(one)):
             bowler_details[bowler][1]+=1
             SecScore+=1
     if(("FOUR" in score or "four" in score) and "wide" not in score):
-        
+        #print score
         bowler_details[bowler][1]+=4
         batsmen_details[batsmen][0]+=4
         batsmen_details[batsmen][1]+=1
         batsmen_details[batsmen][3]+=1
         SecScore+=4
     elif("1" in score and "wide" not in score):
-        
+        #print one[i]
         if(byes=="no"):
             batsmen_details[batsmen][0]+=1
             bowler_details[bowler][1]+=1
@@ -286,7 +151,7 @@ for i in range(1,len(one)):
         
         SecScore+=1
     elif("2" in score and "wide" not in score):
-        
+        #print score
         if(byes=="no"):
             batsmen_details[batsmen][0]+=2
             bowler_details[bowler][1]+=2
@@ -294,7 +159,7 @@ for i in range(1,len(one)):
         
         SecScore+=2
     elif("3" in score and "wide" not in score):
-        
+        #print score
         if(byes=="no"):
             batsmen_details[batsmen][0]+=3
             bowler_details[bowler][1]+=3
@@ -302,37 +167,37 @@ for i in range(1,len(one)):
 
         SecScore+=3
     elif("SIX" in score or "six" in score):
-        
+        #print score
         bowler_details[bowler][1]+=6
         batsmen_details[batsmen][0]+=6
         batsmen_details[batsmen][2]+=1
         batsmen_details[batsmen][3]+=1
         SecScore+=6
     elif(("OUT " in score or "out " in score)and "Run" not in score):
-        
+        #print one[i]
+        #count+=1
+        #print bowler
         bowler_details[bowler][2]+=1
         batsmen_details[batsmen][3]+=1
         SecWickets+=1
     elif("wide" not in score ):
             #print score
             batsmen_details[batsmen][3]+=1
-    
+    #print SecScore,SecWickets
     byes="no"
     try:
         
         if(i>=end):
-            
+            #print second_innings[i-end]
             Sec_details[str(second_innings[i-end])]=[SecScore,SecWickets]
-            
+            #print second_innings[j],SecScore,SecWickets
     except IndexError:
         pass
     j+=1
     if(prev!=bowler):
-        
+        #print i,prev,bowler
         try:
             bowler_details[prev][0]+=1
-            no_ovrs+=1
-            checkf="yes"
         except KeyError:
             a=1
     prev=bowler
@@ -377,15 +242,22 @@ for i in bowler_details:
         
         t.add_row([i,str(bowler_details[i][0]),str(bowler_details[i][1]),str(bowler_details[i][2]),str(round(eco,2))])
 print t
-print "---------------------------------------"
 
-print "Batsmen classification"
-batsmanClassification(batsmen_fclass)
-print "---------------------------------------"
 
-print "Bowler classification"
-bowlerClassification(bowler_fclass)
-print "---------------------------------------"
-
-print "Match prediction"
 DLMethod(target,Sec_details)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
